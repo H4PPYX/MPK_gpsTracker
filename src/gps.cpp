672a54433c8d7a4668aee5c41d3eb14ac97984d5
Kacper <h4ppyx@protonmail.com>
2024-04-24 21:01:41 +0200

    GPS fixes

--- src/gps.cpp
+++ src/gps.cpp
@@ -11,13 +11,13 @@ std::string GPS::getGPSdata(){
 }
 
 void GPS::fetchTime(std::string gpsTime){
-  GPS::time = ("" + gpsTime.substr(0,3) + "-" + gpsTime.substr(4,5) + "-" + gpsTime.substr(6,7) + " " + gpsTime.substr(8,9) + ":" + gpsTime.substr(10,11) + ":" + gpsTime.substr(12,13));
+  GPS::time = ("" + gpsTime.substr(0,4) + "-" + gpsTime.substr(4,2) + "-" + gpsTime.substr(6,2) + " " + gpsTime.substr(8,2) + ":" + gpsTime.substr(10,2) + ":" + gpsTime.substr(12,2));
   return;
 }
 
 void GPS::updateGPSdata() {
-  GPS::gps_lat = "0";
-  GPS::gps_lon = "0";
+  GPS::gps_lat = "0.000000";
+  GPS::gps_lon = "0.000000";
   GPS::fetchTime("20240424182716");
   return;
 }
