14abdb6aadd4f34e650532ba01215575ff20b5aa
Kacper <h4ppyx@protonmail.com>
2024-05-16 20:30:59 +0200

    write data to text file for testing

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "config.h"
 #include "gps.h"
 #include <iostream>
+#include <ofstream>
 
 
 int main() {
@@ -8,12 +9,16 @@ int main() {
   Config conf;
   sim7x00 sim7000 = sim7x00();
   GPS track(sim7000);
+  ofstream file;
+  file.open("output.txt");
 
   while(true){
     track.updateGPSdata();
     std::cout<<track.getGPSdata()<<std::endl;
+    file<<track.getGPSdata()<<std::endl;
     //std::cout<<conf.getVehID()<<";"<<conf.getServIP()<<std::endl;
     delay(1000);
   }
+  file.close();
   return 0;
 }
