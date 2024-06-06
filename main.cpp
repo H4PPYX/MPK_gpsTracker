403fe5d11a9ee5a111c647291cbfd4b74d536e9d
Kacper <h4ppyx@protonmail.com>
2024-06-06 19:24:01 +0200

    connection as interface class

--- main.cpp
+++ main.cpp
@@ -24,6 +24,7 @@ int main() {
     delay(1000);
   }
 
+  ~sender();
   file.close();
   return 0;
 }
--- src/wifiSend.cpp
+++ src/wifiSend.cpp
@@ -1,8 +1,12 @@
 #include "wifiSend.h"
 
-WiFiSend::WiFiSend(sim7x00 device, Config data): sim7000(device), initials(data){}
+WiFiSend::WiFiSend(sim7x00 device, Config data): sim7000(device), initials(data){
+  WiFiSend::send("start;3_00099876");
+}
 
-WiFiSend::~WiFiSend(){}
+WiFiSend::~WiFiSend(){
+  WiFiSend::send("stop;3_00099876");
+}
 
 bool WiFiSend::send(std::string line) {
     system(("curl --header \"Content-Type: text/plain\" --request POST --data \"" + initials.getVehID() + ";" + line + "\" http://" + initials.getServIP() + "/vehiclepositions").c_str());
